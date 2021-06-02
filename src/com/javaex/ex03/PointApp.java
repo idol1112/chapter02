497a8a911acd98d136f239491634f12dd351192f
hi <hi@DESKTOP-OAV2V8U>
2021-06-02 15:55:41 +0900

    연습문제 point

---
+++ src/com/javaex/ex03/PointApp.java
@@ -0,0 +1,23 @@
+package com.javaex.ex03;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Point p1 = new Point();
+		Point p2 = new Point();
+		
+		p1.setX(5);
+		p1.setY(5);
+		
+		p2.setX(10);
+		p2.setY(23);
+		
+		p1.draw();
+		p2.draw();
+		
+		
+	}
+
+}
