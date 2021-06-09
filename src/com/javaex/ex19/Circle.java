f24d41fe6b2b71f9fc3780ae724a4868a3e7fd8c
idol1112 <idol981112@gmail.com>
2021-06-09 15:43:55 +0900

    추상메소드, 추상클래스

---
+++ src/com/javaex/ex19/Circle.java
@@ -0,0 +1,47 @@
+package com.javaex.ex19;
+
+public class Circle extends Shape {
+	
+	//필드
+	private int radius;
+	
+	
+	//생성자
+	public Circle() {
+		
+	}
+
+
+	public Circle(String fillColor, String lineColor, int radius) {
+		super(fillColor, lineColor);
+		this.radius = radius;
+	}
+	
+	
+	
+	//메소드 g/s
+	public int getRadius() {
+		return radius;
+	}
+
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	
+	//메소드-일반
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
+	
+	public void draw() {
+		//[면색:빨강, 선색:검정] 도형을 그렸습니다.
+		System.out.println("[면색:"+fillColor+", 선색:"+lineColor+", 반지름:"+radius+"] 원을 그렸습니다.");
+	}
+	
+	public double area() {
+		return radius*radius*3.14;
+	}
+}
